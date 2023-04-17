523503e43100468d054b3362655858f779ca1e44
Brian Lawrence <Brian@BrianDLawrence.com>
2023-04-17 13:54:16 +0200

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <va-card color="background-element" style="padding: 0.75rem">
+    <h1 class="title">Cats and Poetry create Catetry</h1>
+  </va-card>
+</template>
+<script>
+import { defineComponent } from "@vue/composition-api";
+
+export default {
+  name: "IndexPage",
+  components: {},
+  head() {
+    return {
+      title: "Cats and Poetry is Catetry",
+      meta: [
+        {
+          name: "description",
+          hid: "description",
+          content: "Cats and poetry come together to create Catetry.",
+        },
+      ],
+    };
+  },
+};
+</script>
