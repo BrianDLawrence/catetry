0318325a289c92af271805ffeb81906f2be86bfd
Brian Lawrence <Brian@BrianDLawrence.com>
2023-04-28 11:39:00 +0200

    Updated server api and now fetching example poem

---
+++ pages/poems.vue
@@ -0,0 +1,34 @@
+<template>
+  <i-layout>
+    <i-layout-header class="_text-align:center">
+      <h1>Poems</h1>
+    </i-layout-header>
+
+    <i-layout-content>
+      <p class="_text-align:left">
+        <span style="white-space: pre">{{ data }}</span>
+      </p>
+    </i-layout-content>
+  </i-layout>
+</template>
+
+<script setup lang="ts">
+const { data } = await useFetch("/api/poems", {
+  query: {
+    user: "Brian",
+  },
+});
+
+var value;
+
+useHead({
+  title: "Poems",
+  meta: [
+    {
+      name: "description",
+      content: "Cat poetry is purrfect",
+    },
+  ],
+});
+</script>
+
