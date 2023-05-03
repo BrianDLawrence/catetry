150483db1d9d477a3e73b24023fbaa95aee87d76
Brian Lawrence <Brian@BrianDLawrence.com>
2023-05-03 14:36:51 +0200

    Minor Updates to UI

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,25 +1,39 @@
 <template>
-  <div>
-    <i-navbar>
-      <i-navbar-brand to="/"> CATETRY </i-navbar-brand>
-      <i-navbar-collapsible class="_justify-content:flex-end">
-        <i-nav>
-          <i-nav-item to="/poems"> Poems </i-nav-item>
-          <i-nav-item to="/about"> About </i-nav-item>
-        </i-nav>
-        <i-input placeholder="Type something..">
-          <template #append>
-            <i-button color="secondary">
-              <i-icon name="ink-search" />
-            </i-button>
-          </template>
-        </i-input>
-      </i-navbar-collapsible>
-    </i-navbar>
-    <i-container>
-      <NuxtPage />
-    </i-container>
-  </div>
+  <i-container>
+    <i-layout>
+      <i-layout-header>
+        <i-navbar>
+          <i-navbar-brand to="/"> CATETRY </i-navbar-brand>
+          <i-navbar-collapsible class="_justify-content:flex-end">
+            <i-nav>
+              <i-nav-item to="/poems"> Poems </i-nav-item>
+              <i-nav-item to="/about"> About </i-nav-item>
+            </i-nav>
+            <i-input placeholder="Type something..">
+              <template #append>
+                <i-button color="secondary">
+                  <i-icon name="ink-search" />
+                </i-button>
+              </template>
+            </i-input>
+          </i-navbar-collapsible>
+        </i-navbar>
+      </i-layout-header>
+      <i-layout-content>
+        <NuxtPage />
+      </i-layout-content>
+      <i-layout-footer class="_background:light">
+        <center>
+          <i-nav color="light" class="_justify-content:center">
+            <i-nav-item href="https://inkline.io">Link</i-nav-item>
+            <i-nav-item>Item</i-nav-item>
+            <i-nav-item>Item</i-nav-item>
+            <i-nav-item disabled>Disabled</i-nav-item>
+          </i-nav>
+        </center>
+      </i-layout-footer>
+    </i-layout>
+  </i-container>
 </template>
 <script>
 export default {
