0a39d22e11e8fbd256bc196bba9a8fff2d1a5fe9
Brian Lawrence <brian@briandlawrence.com>
2024-07-19 06:34:45 -0700

    Updated to latest NUXT version + now using GPT4.0 mini for poem generation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,43 +1,49 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    app: {
-        head: {
-            titleTemplate: "Catetry: %s",
-            htmlAttrs: {
-                lang: "en"
-            },
-            bodyAttrs: {
-                class: ["body"]
-            },
-            meta: [
-                { charset: "utf-8" },
-                { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-                {
-                    name: "description",
-                    content: "Cats and poetry come together to create Catetry.",
-                },
-                {
-                    name: "monetag",
-                    content: "e03b3ca405723d150690f57719312483",
-                }
-            ]
+  app: {
+    head: {
+      titleTemplate: "Catetry: %s",
+      htmlAttrs: {
+        lang: "en",
+      },
+      bodyAttrs: {
+        class: ["body"],
+      },
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        {
+          name: "description",
+          content: "Cats and poetry come together to create Catetry.",
         },
-    },
-    modules: ['@nuxtjs/tailwindcss', 'nuxt-gtag', 'nuxt-simple-sitemap', '@nuxtjs/robots'],
-    runtimeConfig: {// availabe only server side
-        openAIKey: process.env.OPENAIKEY,
-        mongoURI: process.env.MONGOURI,
-        BASE_URL: process.env.BASE_URL || 'http://localhost:3000',
-        // Public keys that are exposed to the client
-        public: {
-            siteUrl: 'https://catetry.com',
+        {
+          name: "monetag",
+          content: "e03b3ca405723d150690f57719312483",
         },
+      ],
     },
-    gtag: {
-        id: 'G-MESC18QFLG',
-        initialConsent: false
+  },
+  modules: [
+    "@nuxtjs/tailwindcss",
+    "nuxt-gtag",
+    "@nuxtjs/robots",
+    "@nuxtjs/sitemap",
+  ],
+  runtimeConfig: {
+    // availabe only server side
+    openAIKey: process.env.OPENAIKEY,
+    mongoURI: process.env.MONGOURI,
+    BASE_URL: process.env.BASE_URL || "http://localhost:3000",
+    // Public keys that are exposed to the client
+    public: {
+      siteUrl: "https://catetry.com",
     },
-    sitemap: {
-        siteUrl: 'https://catetry.com',
-    }
-})
+  },
+  gtag: {
+    id: "G-MESC18QFLG",
+    initialConsent: false,
+  },
+  sitemap: {
+    siteUrl: "https://catetry.com",
+  },
+});
--- server/api/generatepoem.get.ts
+++ server/api/generatepoem.get.ts
@@ -2,60 +2,73 @@
  * Utilize openAI to generate a poem based on the Cat
  */
 
-import { Configuration, OpenAIApi } from 'openai'
-const config = useRuntimeConfig()
+import { Configuration, OpenAIApi } from "openai";
+const config = useRuntimeConfig();
 
 export default defineEventHandler(async (event) => {
-
-    if (!config.openAIKey)
-        throw createError({
-            statusCode: 400,
-            statusMessage: "UNDEFINED RUNTIME CONFIGURATION - NO open AI KEY"
-        })
-
-    const query = getQuery(event);
-
-    const question1 = "Can you please create a poem that highlights the good attributes of a cat named " + query.name + "? Please include common attributes of " + query.breed + " cats and additionally the following unique attributes of " + query.name + ":" + query.attributes + "."
-    const question2generic = "I want you to create a poem for cat lovers. Please be creative and choose an appropriate poem type for a cat named:" + query.name + " with the following attributes: " + query.attributes + " Please also include typical cat attributes."
-    const question2specific = "I want you to create a poem for cat lovers. Please be creative and choose an appropriate poem type for a " + query.breed + " cat named:" + query.name + " with the following attributes: " + query.attributes
-    var question = "";
-
-    if (query.breed == "American Shorthair" || query.breed == "other")
-        question = question2generic;
-    else
-        question = question2specific;
-
-    console.log("To Chat GPT:" + question)
-
-    const configuration = new Configuration({
-        apiKey: config.openAIKey
+  if (!config.openAIKey)
+    throw createError({
+      statusCode: 400,
+      statusMessage: "UNDEFINED RUNTIME CONFIGURATION - NO open AI KEY",
     });
 
-    const openai = new OpenAIApi(configuration);
-    try {
+  const query = getQuery(event);
 
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [{ role: "user", content: question }]
-        });
+  const question1 =
+    "Can you please create a poem that highlights the good attributes of a cat named " +
+    query.name +
+    "? Please include common attributes of " +
+    query.breed +
+    " cats and additionally the following unique attributes of " +
+    query.name +
+    ":" +
+    query.attributes +
+    ".";
+  const question2generic =
+    "I want you to create a poem for cat lovers. Please be creative and choose an appropriate poem type for a cat named:" +
+    query.name +
+    " with the following attributes: " +
+    query.attributes +
+    " Please also include typical cat attributes.";
+  const question2specific =
+    "I want you to create a poem for cat lovers. Please be creative and choose an appropriate poem type for a " +
+    query.breed +
+    " cat named:" +
+    query.name +
+    " with the following attributes: " +
+    query.attributes;
+  var question = "";
 
-        console.log(completion.data.choices[0].toString)
+  if (query.breed == "American Shorthair" || query.breed == "other")
+    question = question2generic;
+  else question = question2specific;
 
-        return completion.data.choices[0].message?.content
+  console.log("To Chat GPT:" + question);
 
-    } catch (error: any) {
-        if (error.response) {
-            console.log(error.response.status);
-            console.log(error.response.data);
-        } else {
-            console.log(error.message);
-        }
-        throw createError({
-            statusCode: 400,
-            statusMessage: error.message,
-        })
+  const configuration = new Configuration({
+    apiKey: config.openAIKey,
+  });
+
+  const openai = new OpenAIApi(configuration);
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-4o-mini",
+      messages: [{ role: "user", content: question }],
+    });
+
+    console.log(completion.data.choices[0].toString);
+
+    return completion.data.choices[0].message?.content;
+  } catch (error: any) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
     }
-
-
-
+    throw createError({
+      statusCode: 400,
+      statusMessage: error.message,
+    });
+  }
 });
